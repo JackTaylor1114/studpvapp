e9b40929953af1aa03440573b223a39d6fb11186
Jack Taylor <tobias.grimm.f5o@fh-zwickau.de>
2019-04-11 20:12:41 +0200

    Init with basic spring project

---
+++ src/main/java/de/fhzwickau/studpvapp/StudpvappApplication.java
@@ -0,0 +1,13 @@
+package de.fhzwickau.studpvapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudpvappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudpvappApplication.class, args);
+    }
+
+}
